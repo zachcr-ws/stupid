31537418a3254f5ac47e86b284c81a7787ce9ed3
yz <zhi@verycd.com>
2014-01-27 15:36:55 +0800

    test corntab

--- public/javascripts/stupid.js
+++ public/javascripts/stupid.js
@@ -1623,7 +1623,6 @@ var imgReady = (function () {
         img.src = url;
         img.id = key;
  
-        // 如果图片被缓存，则直接返回缓存数据
         if (img.complete) {
             ready.call(img);
             load && load.call(img);
@@ -1633,19 +1632,16 @@ var imgReady = (function () {
         width = img.width;
         height = img.height;
  
-        // 加载错误后的事件
         img.onerror = function () {
             error && error.call(img);
             onready.end = true;
             img = img.onload = img.onerror = null;
         };
  
-        // 图片尺寸就绪
         onready = function () {
             newWidth = img.width;
             newHeight = img.height;
             if (newWidth !== width || newHeight !== height ||
-                // 如果图片已经在其他地方加载可使用面积检测
                 newWidth * newHeight > 1024
             ) {
                 ready.call(img);
@@ -1654,22 +1650,16 @@ var imgReady = (function () {
         };
         onready();
  
-        // 完全加载完毕的事件
         img.onload = function () {
-            // onload在定时器时间差范围内可能比onready快
-            // 这里进行检查并保证onready优先执行
             !onready.end && onready();
  
             load && load.call(img);
  
-            // IE gif动画会循环执行onload，置空onload即可
             img = img.onload = img.onerror = null;
         };
  
-        // 加入队列中定期执行
         if (!onready.end) {
             list.push(onready);
-            // 无论何时只允许出现一个定时器，减少浏览器性能损耗
             if (intervalId === null) intervalId = setInterval(tick, 40);
         };
     };
